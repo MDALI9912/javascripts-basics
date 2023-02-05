1a966374822f444c6bda6900376098054faf91dd
md shafayath ali <118418572+786shafayath786@users.noreply.github.com>
2023-02-06 01:13:31 +0530

    basic node server

--- 0014-node-server/index.js
+++ 0014-node-server/index.js
@@ -1,21 +1,25 @@
-// HTTP Server module
-// NodeJS Inbuil module (http)
+// HTTP Server Module
+// NodeJS InBuild Module (http)
 
 
 
 const http = require("http");
 
-
-    // const server = http.createserver(function (request, response) {
-    // response.writehead(200);
-    // response.end("data server from node server")
-    // });
-    
-    const requestlistener = function(request, response) {
-        response.writehead(200);
-        response.end("data from node server");
-    }
-    const server = http.createserver(requestlistener);
+// const server = http.createServer(function (request, respones) {
+// respones.writeHead(200);
+// respones.end("data server from node server")
+// });
 
 
-   
+
+
+const requestListener = function(request, response) {
+    response.writeHead(200);
+    response.end("This is Mohammed Shafayath Ali ");
+}
+
+   const server = http.createServer(requestListener);
+
+   server.listen(80, 'localhost', () => {
+    console.log('ali server is running.....')
+   });
