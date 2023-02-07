c3d734688cc78970ce42334a0a69eb504fcced6d
md shafayath ali <118418572+786shafayath786@users.noreply.github.com>
2023-02-07 13:05:57 +0530

    data-base

---
+++ 2-data-structure/1-arrays.js
@@ -0,0 +1,33 @@
+// Arrays starts with []
+// Array is used define multiple value (string, numbers, booleans, array, objects)
+
+[1,2,3,4,5] //-> [1, 2, 3, 4, 5]
+ [1, 2, 3, 4, 5]  //  -> [1, 2, 3, 4, 5]
+ [
+    1,
+    2,
+    3,
+    4,
+    5
+ ]          // -> [1, 2, 3, 4, 5]
+
+
+     ['md','ali','shafayath','jahid','salam']
+   //  0     1        2         3        4   in java script will start with 0 fist
+
+
+     ['string','100',parseInt('23k'),56 > 100, ['a','b','c'] ]
+  // ['string','100',      23,         fales,   ['a','b','c'] ]
+  //     0      1           2            3          4
+
+
+  ['string','100',parseInt('23k'),56 > 100, ['a','b','c'] ][4][2]
+  // ans ['a', 'b','c']
+  // ans 'c'
+
+
+  //[][POSITION] -> VALUE
+  //[][FOURTH - 1] -> VALUE
+  //[][4 - 1] -> VALUE
+  //[][3] -> VALUE
+  
---
+++ 2-data-structure/2-objects.js
@@ -0,0 +1,11 @@
+({
+    "NAME": "SHAFAYATH",
+desugnation: "developer",
+frendes: ["ali","salam","jahid"],
+married: false,
+companey: {
+    name: "SAP",
+    position: "uI Designer"
+}
+  "full name": "mohammed shafayath ali"
+})['NAME']
