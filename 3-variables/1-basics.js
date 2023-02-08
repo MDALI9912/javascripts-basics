05054b551a4152db670e54551e1f534ff4d0ed12
md shafayath ali <118418572+786shafayath786@users.noreply.github.com>
2023-02-09 01:11:29 +0530

    variables basics added

---
+++ 3-variables/1-basics.js
@@ -0,0 +1,40 @@
+// 1. Declaration
+// No value assigned to myName identifier (variable)
+var myName;
+
+// 2. assigning value (any datatype) to myName identifier
+myName = "md ali";
+
+
+// Delaring variable and assigning value to it
+var fullname = 'md ali'
+
+
+var firstName = 'md';
+var middleName = 'shafayath';
+var lastName = 'ali'
+
+var myfullName = firstName + middleName + lastName
+
+
+// in one var we had to number of  striengs or etc for that we had to use this formate
+
+//var myName = firstName +" "+ middleName + " "+ lastName
+var myName = `${firstName} ${middleName} ${lastName}`
+
+// answer : "md shafayath ali"
+
+// after runing this form if we call firstname then  'md' will come that is main use of variables 
+
+var city ='guntur';
+var state = 'ap';
+var country = "india";
+
+var addres = `${city} ,${state} ${country}`;
+
+// lowercase (small letter)x
+var x = 200 + 99;
+// uppercase (capital letter)X
+var X = 89 + 78 / 10;
+
+
