6f9ea6f54415206022f14d808b5765807abd55f7
md shafayath ali <118418572+786shafayath786@users.noreply.github.com>
2023-02-10 01:52:42 +0530

    added greet user added

---
+++ 4-first-program/greet-user.js
@@ -0,0 +1,25 @@
+console.log("Hello user");
+
+var firstName = "md";
+var lastName = "shafayath";
+
+
+console.log(`${firstName} ${lastName}`);  // this is important 
+
+
+console.log("***********************************");
+console.log(` 2 * 1 = ${2*1} `);
+console.log(` 2 * 2 = ${2*2} `);
+console.log(` 2 * 3 = ${2*3} `);
+console.log(` 2 * 4 = ${2*4} `);
+console.log(` 2 * 5 = ${2*5} `);
+console.log(` 2 * 6 = ${2*6} `);
+console.log(` 2 * 7 = ${2*7} `);
+console.log(` 2 * 8 = ${2*8} `);
+console.log(` 2 * 9 = ${2*9} `);
+console.log(` 2 * 10 = ${2*10} `);
+
+console.log("************************************");
+
+
+
