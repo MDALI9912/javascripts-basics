433f7fa1dd039e7483b0034fd38faf61e4e36716
md shafayath ali <118418572+786shafayath786@users.noreply.github.com>
2023-02-09 23:59:46 +0530

    my-first-program

---
+++ 4-first-program/index.js
@@ -0,0 +1,33 @@
+var intial = "md";
+let firstName = "shafayath";
+let lastName = "ali";
+
+const city = "Guntur";
+
+const friend = [
+    'salam',
+    'jahide',
+    'sameer'
+];
+
+
+const myDetails = {
+    fullname: '${firstName} ${lastName}, ${intial}',
+    designation : 'frontend Dev',
+    friends : friend,
+    city : city
+}
+
+console.log(myDetails);
+
+
+
+// javascript Runtime Environment
+// 2. Browser
+// 1. node
+
+
+// Run this file in NODE environement
+// example : node FILENAME
+//node index.js
+
